4015875308db76a25ca5cf5741ae0191d5700a06
andrewmanin <andrejmanin54@gmail.com>
2024-11-29 00:32:25 +0200

    Innit commit

--- client/main.cpp
+++ client/main.cpp
@@ -1,35 +1,32 @@
+#include <iostream>
+
 #include "core/client.h"
 
-int main(int argc, char* argv[]) {
+int main() {
     try {
-        if(argc != 4) {
-            std::cerr << "Usage: chat_client <host> <port> <nickname>\n";
-            return 1;
-        }
         boost::asio::io_context io_context;
         tcp::resolver resolver(io_context);
-        tcp::resolver::query query(argv[1], argv[2]);
-        tcp::resolver::iterator iterator = resolver.resolve(query);
-        std::array<char, MAX_NICKNAME> nickname{};
-        strcpy(nickname.data(), argv[3]);
+        resolver.resolve(tcp::v4(), "127.0.0.1", std::to_string(PORT));
+        Client client(io_context, resolver);
 
-        Client client(nickname, io_context, iterator);
-        std::thread t([&io_context]() {
-            io_context.run();
-        });
-        std::array<char, MAX_IP_PACK_SIZE> msg{};
-        while(true) {
-            memset(msg.data(), '\0', msg.size());
-            if(!std::cin.getline(msg.data(), MAX_IP_PACK_SIZE - MAX_NICKNAME)) {
-                std::cin.clear();
+        std::thread t([&io_context](){ io_context.run(); });
+
+        std::thread input_thread([&client]() {
+            std::string input;
+            while (std::getline(std::cin, input)) {
+                if (!input.empty()) {
+                    std::array<char, MAX_IP_PACK_SIZE> msg{};
+                    std::copy(input.begin(), input.end(), msg.begin());
+                    client.write(msg);
+                }
             }
-            client.write(msg);
-        }
+        });
 
-        client.close();
+        input_thread.join();
         t.join();
-    }catch(std::exception &ex) {
-        std::cerr << "Exception: " << ex.what() << std::endl;
+
+
+    } catch (std::exception& exception) {
+        std::cerr << "Exception: " << exception.what() << std::endl;
     }
-    return 0;
 }
--- protocol.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROTOCOL_H
-#define PROTOCOL_H
-
-constexpr int MAX_IP_PACK_SIZE = 512;
-constexpr short MAX_NICKNAME = 16;
-
-#endif //PROTOCOL_H
